4ff8a9d64222924fdea865de995a223956302264
Jay Fuller <jason.fuller@nyu.edu>
2017-10-19 22:08:20 -0400

    fixed path to dataabae

--- updateBabysleep.py
+++ updateBabysleep.py
@@ -12,7 +12,7 @@ os.chdir("/users/heegeradmin/internal/ParticipantDataDumps/")
 list_of_files = glob.glob('webform_views_register_your_baby_for_the_research*') # * means all if need specific format then *.csv
 inFileWeb = max(list_of_files, key=os.path.getctime) # get the latest file
 inFileApp = sys.argv[1] # /users/heegeradmin/internal/babysleepAppData
-inFileDB = '/users/heegeradmin/internal/babysleep.db'
+inFileDB = '/users/heegeradmin/internal/babysleepDatabase/babysleep.db'
 outFile = '/users/heegeradmin/Sites/sites/dashboard/assets/data/'
 
 # sys.exit()
