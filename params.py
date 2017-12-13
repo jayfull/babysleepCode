0b4e24ea5f3b1f825c2dc33645d87fda6fbae11c
Jay Fuller <jason.fuller@nyu.edu>
2017-12-13 17:33:41 -0500

    created global parameters file

---
+++ params.py
@@ -0,0 +1,17 @@
+import pickle
+
+shared = {
+"Foo":"Bar", 
+"Parrot":"Dead"
+}
+
+fp = open("shared.pkl","w")
+pickle.dump(shared, fp)
+
+
+
+# import pickle
+
+# fp = open("shared.pkl")
+# shared = pickle.load(fp)
+# print shared["Foo"]
