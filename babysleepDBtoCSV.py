6ffb825bbd8f3b8ded676e540d3140fe93581830
Jay Fuller <jason.fuller@nyu.edu>
2017-12-12 21:15:22 -0500

    adding script to export the entire DB is csv

---
+++ babysleepDBtoCSV.py
@@ -0,0 +1,20 @@
+import sqlite3
+import csv
+
+inFileDB = '/users/heegeradmin/internal/babysleepDatabase/babysleep.db' #data base location
+
+csvWriter = csv.writer(open("babysleepDB.csv", "w")) # output file for csv dump
+
+# create new db and make connection
+conn = sqlite3.connect(inFileDB)
+conn.text_factory = str
+c = conn.cursor()
+
+# SQL query to select all rows in database
+c.execute('SELECT * \
+	FROM Kids') 
+rows = c.fetchall() 
+
+os.chdir('/users/heegeradmin/internal/babysleepDatabase/')
+
+csvWriter.writerows(rows) # write out the query to the csv
