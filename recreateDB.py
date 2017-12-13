d811f78216a7aa8a521b41fa56438fce6bf16c2f
Jay Fuller <jason.fuller@nyu.edu>
2017-12-12 22:58:29 -0500

    added recreateDB.py to recreate babysleepDB.py from scratch

---
+++ recreateDB.py
@@ -0,0 +1,24 @@
+import sqlite3
+import pandas as pd
+import numpy as np
+import glob
+import os
+import sys
+
+inFileDB = sys.argv[1] # name of the DB must be given on the command line
+
+# create new db and make connection
+conn = sqlite3.connect(inFileDB)
+conn.text_factory = str
+c = conn.cursor()
+
+import glob
+files = glob.glob("/users/heegeradmin/internal/babysleepAppData/*.csv")
+
+for file in files[0]:
+	sys.argv = [file]
+	execfile("updateBabysleep.py")
+
+
+# close the database connection
+conn.close() # close the connection to the database
