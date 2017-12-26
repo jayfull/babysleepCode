ce03bb3b4e25718e6b0140aafbd9ae01978746ea
Jay Fuller <jason.fuller@nyu.edu>
2017-12-26 02:38:30 -0500

    updated paths in updatebabysleep and increased the number of files to test in recreate babysleep

--- recreateDB.py
+++ recreateDB.py
@@ -22,7 +22,7 @@ c = conn.cursor()
 
 
 os.chdir("/users/heegeradmin/internal/babysleepCode/")
-for inFileApp in inFilesApp[:1]:
+for inFileApp in inFilesApp[:10]:
 	sys.argv = [inFileWeb, inFileApp]
 	print sys.argv
 	execfile("dataToDB.py")
--- updateBabysleep.py
+++ updateBabysleep.py
@@ -10,10 +10,11 @@ import sys
 ######################
 os.chdir("/users/heegeradmin/internal/ParticipantDataDumps/")
 list_of_files = glob.glob('webform_views_register_your_baby_for_the_research*') # * means all if need specific format then *.csv
-inFileWeb = max(list_of_files, key=os.path.getctime) # get the latest file
+inFileWeb = "/users/heegeradmin/internal/ParticipantDataDumps/" + max(list_of_files, key=os.path.getctime) # get the latest file
 inFileApp = sys.argv[1] # /users/heegeradmin/internal/babysleepAppData
 inFileDB = '/users/heegeradmin/internal/babysleepDatabase/babysleep.db'
-outFile = '/users/heegeradmin/Sites/sites/dashboard/assets/data/'
+# outFile = '/users/heegeradmin/Sites/sites/dashboard/assets/data/'
+outFile = '/users/heegeradmin/internal/babysleepDatabase/'
 
 # sys.exit()
 
@@ -22,9 +23,14 @@ outFile = '/users/heegeradmin/Sites/sites/dashboard/assets/data/'
 ###### Data To Database ######
 ##############################
 
-os.chdir("/users/heegeradmin/internal/babysleepCode/")
+# create new db and make connection
+conn = sqlite3.connect(inFileDB)
+conn.text_factory = str
+c = conn.cursor()
 
 # Read, merge and write data to DB
+sys.argv = [inFileWeb, inFileApp]
+os.chdir("/users/heegeradmin/internal/babysleepCode/")
 execfile("dataToDB.py")
 
 
