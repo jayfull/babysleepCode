dd7209a0693800ddc40d424bce2bdc247d6b401f
Jay Fuller <jason.fuller@nyu.edu>
2018-01-03 21:28:08 -0500

    import json

--- recreateDB.py
+++ recreateDB.py
@@ -4,6 +4,7 @@ import numpy as np
 import glob
 import os
 import sys
+import json
 
 
 os.chdir("/users/heegeradmin/internal/ParticipantDataDumps/")
--- updateBabysleep.py
+++ updateBabysleep.py
@@ -4,6 +4,7 @@ import numpy as np
 import glob
 import os
 import sys
+import json
 
 ######################
 ####### Inputs #######
