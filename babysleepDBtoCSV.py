8c97426b879b15488dcf4d274534482cf53fe525
Jay Fuller <jason.fuller@nyu.edu>
2018-01-24 19:42:46 -0500

    fixed directory pointer

--- babysleepDBtoCSV.py
+++ babysleepDBtoCSV.py
@@ -2,7 +2,7 @@ import sqlite3
 import csv
 import os
 
-# os.chdir('/users/heegeradmin/internal/babysleepDatabase/')
+os.chdir('/users/heegeradmin/internal/babysleepDatabase/')
 
 # inFileDB = '/users/heegeradmin/internal/babysleepDatabase/babysleep.db' #data base location
 
@@ -21,4 +21,4 @@ import os
 
 # csvWriter.writerows(rows) # write out the query to the csv
 
-os.system("sqlite3 -header -csv babysleep.db "select * from Kids;" > database.csv")
+os.system("sqlite3 -header -csv babysleep.db "select * from Kids;" > babysleepDB.csv")
