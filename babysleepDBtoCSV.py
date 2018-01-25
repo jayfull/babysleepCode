880c461df8d7f34dc9b17e4e2012c4920aae981a
Jay Fuller <jason.fuller@nyu.edu>
2018-01-24 19:40:06 -0500

    changed db to csv to be a bash command

--- babysleepDBtoCSV.py
+++ babysleepDBtoCSV.py
@@ -2,21 +2,23 @@ import sqlite3
 import csv
 import os
 
-os.chdir('/users/heegeradmin/internal/babysleepDatabase/')
+# os.chdir('/users/heegeradmin/internal/babysleepDatabase/')
 
-inFileDB = '/users/heegeradmin/internal/babysleepDatabase/babysleep.db' #data base location
+# inFileDB = '/users/heegeradmin/internal/babysleepDatabase/babysleep.db' #data base location
 
-csvWriter = csv.writer(open("babysleepDB.csv", "w")) # output file for csv dump
+# csvWriter = csv.writer(open("babysleepDB.csv", "w")) # output file for csv dump
 
-# create new db and make connection
-conn = sqlite3.connect(inFileDB)
-conn.text_factory = str
-c = conn.cursor()
+# # create new db and make connection
+# conn = sqlite3.connect(inFileDB)
+# conn.text_factory = str
+# c = conn.cursor()
 
-# SQL query to select all rows in database
-c.execute('SELECT * \
-	FROM Kids') 
-rows = c.fetchall() 
+# # SQL query to select all rows in database
+# c.execute('SELECT * \
+# 	FROM Kids') 
+# rows = c.fetchall() 
 
 
-csvWriter.writerows(rows) # write out the query to the csv
+# csvWriter.writerows(rows) # write out the query to the csv
+
+os.system("sqlite3 -header -csv babysleep.db "select * from Kids;" > database.csv")
